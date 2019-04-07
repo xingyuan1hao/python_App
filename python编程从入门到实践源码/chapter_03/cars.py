9fc7161bb96c0f61c997f1298b9060da671d0a4a
@Lin <ahhlin@163.com>
2019-04-07 23:08:13 +0800

    Update cars.py

--- python编程从入门到实践源码/chapter_03/cars.py
+++ python编程从入门到实践源码/chapter_03/cars.py
@@ -1,8 +1,11 @@
+
+//组织列表-sort/打印
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 
 print("Here is the original list:")
 print(cars)
 
+//临时排序sorted
 print("\nHere is the sorted list:")
 print(sorted(cars))
 
